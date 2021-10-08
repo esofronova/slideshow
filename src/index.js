cd9177600ec2c6c6ff184661cb2e7a5866bd1575
Elena <84809781+esofronova@users.noreply.github.com>
2021-10-08 12:11:45 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './custom-classes.scss';
+import './classes.scss';
 import App from './app';
 
 ReactDOM.render(
